971b07f715d4d568bd0171a8a72f1ffedb032d2e
cynthia eddy'git congif -- global user.email cynthiaeddynyc@gmail.comgit config --global user.name cynthia <cynthiaeddynyc@gmail.com>
2019-12-17 15:53:51 -0500

    responsive navbar done

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
 import React from 'react';
 import './App.css';
+import Navbar from './components/navbar/Navbar'
 
 function App() {
   return (
     <div className='container'>
-
-      <ul className='navBar'>
-  <li><a href="#">Home</a></li>
-  <li><a href="#">About</a></li>
-  <li><a href="#">Products</a></li>
-  <li><a href="#">Contact</a></li>
-</ul>
+      <Navbar/>
 
 
-    
+
+
+
+
 
     <div className="wrapper">
-  <header className="header">Header</header>
   <aside className="aside aside-1">Aside 1</aside>
   <article className="main">
     <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import './Navbar.css'
+
+
+const Navbar = () => {
+
+  return (<div>
+      <nav id="navbar">
+
+
+        <h2>
+          <li>
+            <Link to="/" className='home'>Home</Link>
+          </li>
+          <li>
+            <Link to="/projects" className='projects'>Projects</Link>
+          </li>
+          <li>
+            <Link to="/about-me" className='about-me'>About Me</Link>
+          </li>
+        </h2>
+        
+
+
+      </nav>
+
+    </div>)
+  }
+
+
+
+
+  export default Navbar
