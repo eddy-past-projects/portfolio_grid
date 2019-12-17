3985d6dd986489cc20b93345b6a1afabd0034278
cynthia eddy'git congif -- global user.email cynthiaeddynyc@gmail.comgit config --global user.name cynthia <cynthiaeddynyc@gmail.com>
2019-12-17 16:55:41 -0500

    swirl works, centered in column

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import './App.css';
+import './logo.css';
 import Navbar from './components/navbar/Navbar'
+import swirl_sm from './assets/swirl_sm.jpg'
+
+
 
 function App() {
   return (
@@ -14,9 +18,12 @@ function App() {
 
 
     <div className="wrapper">
-  <aside className="aside aside-1">Aside 1</aside>
+  <aside className="aside logo">
+    <img src={swirl_sm} className="swirl" alt="logo"/>
+
+  </aside>
   <article className="main">
-    <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
+    <p>Pellentesque habitant morbi trisbitant morbi tristique senectus et netus et malesuada fames ac turbitant morbi tristique senectus et netus et malesuada fames ac turbitant morbi tristique senectus et netus et malesuada fames ac turbitant morbi tristique senectus et netus et malesuada fames ac turbitant morbi tristique senectus et netus et malesuada fames ac turbitant morbi tristique senectus et netus et malesuada fames ac turbitant morbi tristique senectus et netus et malesuada fames ac turbitant morbi tristique senectus et netus et malesuada fames ac turbitant morbi tristique senectus et netus et malesuada fames ac turbitant morbi tristique senectus et netus et malesuada fames ac turbitant morbi tristique senectus et netus et malesuada fames ac turbitant morbi tristique senectus et netus et malesuada fames ac turbitant morbi tristique senectus et netus et malesuada fames ac turbitant morbi tristique senectus et netus et malesuada fames ac turbitant morbi tristique senectus et netus et malesuada fames ac turbitant morbi tristique senectus et netus et malesuada fames ac turbitant morbi tristique senectus et netus et malesuada fames ac turbitant morbi tristique senectus et netus et malesuada fames ac turbitant morbi tristique senectus et netus et malesuada fames ac turbitant morbi tristique senectus et netus et malesuada fames ac turbitant morbi tristique senectus et netus et malesuada fames ac turbitant morbi tristique senectus et netus et malesuada fames ac turbitant morbi tristique senectus et netus et malesuada fames ac turbitant morbi tristique senectus et netus et malesuada fames ac turtique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
   </article>
 
   <footer className="footer">Footer</footer>
