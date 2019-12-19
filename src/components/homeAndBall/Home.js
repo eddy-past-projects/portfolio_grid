83c24dcb9643e04a10df568d2c292f791ed371aa
cynthia eddy'git congif -- global user.email cynthiaeddynyc@gmail.comgit config --global user.name cynthia <cynthiaeddynyc@gmail.com>
2019-12-19 00:26:35 -0500

    adding home page

---
+++ src/components/homeAndBall/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import BounceBall from './BounceBall'
+import './Home.css'
+
+const Home = () => {
+  return (<div >
+
+    <BounceBall/>
+  </div>)
+
+}
+export default Home
