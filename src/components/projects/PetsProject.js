2a89899ab047775f309d8e75b79cadbd9d73dcf4
cynthia eddy'git congif -- global user.email cynthiaeddynyc@gmail.comgit config --global user.name cynthia <cynthiaeddynyc@gmail.com>
2020-02-20 22:48:57 -0500

    responsive video

--- src/components/projects/PetsProject.js
+++ src/components/projects/PetsProject.js
@@ -1,30 +1,32 @@
 import React from 'react'
-import pets from '../../assets/pets.jpg'
+import ReactPlayer from 'react-player'
 
 
 
 
 
 
-const PetsProject = () =>{
+
+const PetsProject = () => {
 
   return (
-    <div className='solo-card'>
-      <a href="https://adopt-a-pet.netlify.com/">
-  <img src={pets} className='project-solo' alt='puppy'/> </a>
+    <div className='solo-card pet-project'>
+      <div className='player-wrapper'>
+
+      <ReactPlayer url='https://youtu.be/4kldC32EnBk' controls={true}
+        className='react-player'
+        playing
+        width='100%'
+        height='100%'
+/>
 
 
-
-
-
-
-  <h3><br/>Adopt A Pet! is an app for adopting/maintaining a virtual pet without the upkeep of a live one • Persists each user to a Postgres database
-    • Presently MVP, rebuilt from a Single Page Application with Javascript to React, Redux frontend and a Rails API
+  <h3>Adopt A Pet is an app for adopting/maintaining a virtual pet without the upkeep of a live one • Persists each user to a Postgres database
+    • Presently MVP, rebuilt from a Single Page Application with Javascript to React frontend and a Rails API with JWT authorization
     • Employs flexbox with hamburger menu<br/>
   </h3>
 </div>
-
-
+</div>
   )
 }
 
