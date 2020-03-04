d1da1b4180bf90bceac99561393017f10b0110f6
cynthia eddy'git congif -- global user.email cynthiaeddynyc@gmail.comgit config --global user.name cynthia <cynthiaeddynyc@gmail.com>
2020-03-04 15:01:26 -0500

    changed order cards

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -15,8 +15,8 @@ class Projects extends React.Component {
     return (
       <div className='text'>
         <div className='cards'>
+          <Link to='/pets'><img src={pets} className='project-card' alt=''/></Link>
             <Link to='/kayak'><img src={kayaksWh} className='project-card' alt=''/></Link>
-            <Link to='/pets'><img src={pets} className='project-card' alt=''/></Link>
             <Link to='/abode'><img src={abode} className='project-card' alt=''/></Link>
 
           </div>
