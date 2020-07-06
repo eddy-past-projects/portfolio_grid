4d938ea183309600a806fc0752c5936ee6d9ac7f
cynthia eddy'git congif -- global user.email cynthiaeddynyc@gmail.comgit config --global user.name cynthia <cynthiaeddynyc@gmail.com>
2020-07-06 16:50:27 -0400

    fixing mobile

--- src/components/aboutMe/AboutMe.js
+++ src/components/aboutMe/AboutMe.js
@@ -1,39 +1,44 @@
-import React from 'react'
-import linkedin2 from '../../assets/linkedin2.jpg'
-import ceddy_resume from '../../assets/ceddy_resume.pdf'
-import './AboutMe.css'
+import React from 'react';
+import linkedin2 from '../../assets/linkedin2.jpg';
+import ceddy_resume from '../../assets/ceddy_resume.pdf';
+import './AboutMe.css';
 
 const AboutMe = () => {
+	return (
+		<div>
+			<div className="text">
+				<div className="image">
+					<img src={linkedin2} className="my-photo" alt="" />
+				</div>
+				<div className="filler-box" />
 
-  return (<div>
+				<h1>I've been around!</h1>
+				<h3>
+					Before the metamorphosis of the internet, I was an editorial art director/designer for major
+					publishing houses here in nyc. As the internet evolved, the plethora of magazines diminished and the
+					usage of the revolving door increased. I broadened my skillset and became adept in designing for the
+					iPad, iPhone.
+				</h3>
+				<h1>(and around).</h1>
+				<h3>
+					I recently graduated from the immersive software engineering program at Flatiron School. I'm eager
+					to merge my newly acquired skills with the common thread of engaging the user with visually
+					interesting solutions.
+				</h3>
+				<br />
 
-    <div className='text'>
+				<a href={ceddy_resume}>
+					<h5>click here to view my resume </h5>
+				</a>
+				<a href="http://www.cynthiaeddy.com">
+					<h5>| my print and digital portfolio </h5>
+				</a>
+				<a href="mailto:cynthiaeddynyc@gmail.com">
+					<h5>| email </h5>
+				</a>
+			</div>
+		</div>
+	);
+};
 
-      <div className='image'>
-        <img src={linkedin2} className="my-photo" alt=""/>
-      </div><br/><br/><br/><br/><br/><br/><br/><br/>
-
-      <h1>I've been around!
-      </h1>
-      <h3>Before the metamorphosis of the internet, I was an editorial art director/designer for major publishing houses here in nyc. As the internet evolved, the plethora of magazines diminished and the usage of the revolving door increased. I broadened my skillset and became adept in designing for the iPad, iPhone.
-      </h3>
-      <h1>(and around).
-      </h1>
-      <h3>I recently graduated from the immersive software engineering program at Flatiron School. I'm eager to merge my newly acquired skills with the common thread of engaging the user with visually interesting solutions.</h3><br/>
-
-
-    <a href={ceddy_resume} >
-      <h5>click here to view my resume </h5>
-    </a>
-    <a  href="http://www.cynthiaeddy.com">
-      <h5>| my print and digital portfolio </h5>
-    </a>
-    <a href="mailto:cynthiaeddynyc@gmail.com">
-      <h5>| email </h5>
-    </a>
-      </div>
-
-  </div>)
-}
-
-export default AboutMe
+export default AboutMe;
--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,13 +1,13 @@
-import React from 'react'
-import './Home.css'
-import './homeLogotry.css'
-import Projects from '../projects/Projects'
-import '../projects/Projects.css'
+import React from 'react';
+import './Home.css';
+import Projects from '../projects/Projects';
+import '../projects/Projects.css';
 
 const Home = () => {
-  return (<div >
-    <Projects/>
-  </div>)
-
-}
-export default Home
+	return (
+		<div>
+			<Projects />
+		</div>
+	);
+};
+export default Home;
