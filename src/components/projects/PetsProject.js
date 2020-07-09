bf8edac2a7fbefe83bdf146d87e3c6c221984b91
cynthia eddy'git congif -- global user.email cynthiaeddynyc@gmail.comgit config --global user.name cynthia <cynthiaeddynyc@gmail.com>
2020-07-08 21:01:14 -0400

    styled pets text

--- src/components/projects/PetsProject.js
+++ src/components/projects/PetsProject.js
@@ -10,9 +10,16 @@ const PetsProject = () => {
 
 				<h3>
 					<strong>Adopt A Pet</strong> is an app for adopting/maintaining a virtual pet without the upkeep of
-					a live one<br />• Persists each user to a Postgres database<br />• Presently MVP, rebuilt from a
-					Single Page Application with Javascript to React frontend and a Rails API with JWT authorization<br />•
-					Employs flexbox with hamburger menu<br />
+					a live one
+					<ul>
+						<li>Persists each user to a Postgres database</li>
+
+						<li>
+							Presently MVP, rebuilt from a Single Page Application with Javascript to React frontend and
+							a Rails API with JWT authorization
+						</li>
+						<li>Employs flexbox with hamburger menu</li>
+					</ul>
 				</h3>
 				<div className="icons">
 					<a href="https://github.com/zoey7117/petstoo_frontend/tree/stylini">
