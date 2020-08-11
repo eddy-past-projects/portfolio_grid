aec60f88ac2e251557b1b907355215f0f977f7d7
cynthia eddy'git congif -- global user.email cynthiaeddynyc@gmail.comgit config --global user.name cynthia <cynthiaeddynyc@gmail.com>
2020-08-10 21:47:49 -0400

    roll not working yet

--- src/components/splash/Splash.js
+++ src/components/splash/Splash.js
@@ -1,25 +1,18 @@
-import React from 'react'
-import '../home/myLogoRoll.css'
-import swirl_sm from '../../assets/swirl_sm.jpg'
-import '../home/Home.css'
-
-
-
-
-
+import React from 'react';
+import '../home/myLogoRoll.css';
+import swirl_sm from '../../assets/swirl_sm.jpg';
+import '../home/Home.css';
 
 const Splash = () => {
-  return (<div >
-    <div className='home-image'>
-      <div className="homeLogo swirl_sm">
-        <img src={swirl_sm} className="swirl" alt="logo"/>
+	return (
+		<div>
+			{/* <div className='home-image'> */}
+			<div className="homeLogo swirl_sm">
+				<img src={swirl_sm} className="swirl" alt="logo" />
+			</div>
 
-
-      </div>
-
-</div>
-
-  </div>)
-
-}
-export default Splash
+			{/* </div> */}
+		</div>
+	);
+};
+export default Splash;
